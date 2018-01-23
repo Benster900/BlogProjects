cc30ac51272f060cae41b8487893b69b9e2eb5d7
Ben Bornholm <bdb6115@rit.edu>
2018-01-22 21:07:43 -0500

    WinRMize lab computers script

---
+++ LabScrips/README.md
@@ -0,0 +1,6 @@
+# Lab scripts
+These are scripts that I use in my lab enviornment at school. Scripts are created to accomplish a simple task and NOT for security. Please use at your own risk.
+
+# WinRMize Windows clients for Ansible
+0. Spin up Windows host
+0. ./psexec_ansiblize_setup.ps1 -u <username>
---
+++ LabScrips/WinRMize/psexec_ansiblize_setup.ps1
@@ -0,0 +1,51 @@
+<#
+Author: Ben Bornohlm
+Project: Blog scripts
+Date: 1-22-18
+#>
+
+<#
+COmmand line parameters
+#>
+Param
+(
+  [Parameter(Mandatory=$true)][string]$username,
+  [string]$password = $( Read-Host "Input password, please" )
+)
+
+# Array for jobs
+$jobs=@()
+
+# Mount drive
+NET USE M: \\live.sysinternals.com
+
+Write-Host $username
+Write-Host $password
+
+<#
+Script block to run PsExec on each machine
+#>
+$sb = {
+  param
+  (
+      $computer = "",
+      $username = "",
+      $password = "",
+      $command = "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command `"iex ((new-object net.webclient).DownloadString('https://github.com/ansible/ansible/raw/devel/examples/scripts/ConfigureRemotingForAnsible.ps1'))"
+  )
+
+  if (( Test-Connection -Cn $computer -BufferSize 16 -Count 1 -ea 0 -TimeToLive 3 -quiet )) {
+    M:\PsExec "\\$computer" -u $username -p $password -d cmd /c $Command
+  }
+}
+
+<#
+Iterate over each compunter in hosts file
+#>
+foreach ($computer in Get-Content hosts) {
+  Write-Host "starting $computer" -foreground green
+  $jobs += Start-Job $sb -ArgumentList $computer, $username, $password
+}
+
+Wait-Job $jobs
+$jobs | Receive-Job
